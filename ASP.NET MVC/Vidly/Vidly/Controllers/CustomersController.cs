e8e58175c83baa40faaa3b18403e63dfae50c912
Atomneo <neo1k4+github@gmail.com>
2017-07-11 01:19:18 +0200

    New Customer View

--- ASP.NET MVC/Vidly/Vidly/Controllers/CustomersController.cs
+++ ASP.NET MVC/Vidly/Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -40,7 +41,13 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes,
+
+            };
+            return View(viewModel);
         }
     }
 }
