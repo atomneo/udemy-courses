bd32fe4352da95d002bfa558ee3015a0144d4881
Atomneo <neo1k4+github@gmail.com>
2017-07-10 03:38:45 +0200

    section 2

--- ASP.NET MVC/Vidly/Vidly/Controllers/MoviesController.cs
+++ ASP.NET MVC/Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -16,7 +17,48 @@ namespace Vidly.Controllers
             {
                 Name = "Tylko mnie kochaj"
             };
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: /Movies/Edit/id
+        public ActionResult Edit(int id)
+        {
+            return Content($"Id: {id}");
+        }
+
+        // GET: /movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content($"pageIndex={pageIndex}, sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year:regex(\\d{4}):range(1800, 2100)}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}-{month}");
         }
     }
 }
